04dad8711256b50e62cea898914938a1359d078f
cyndizhao <cyndi.zhao.sfu@gmail.com>
2017-06-27 20:51:36 -0700

    Add videolist

---
+++ src/components/video_list.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import VideoListItems from './video_list_item';
+
+const VideoList = (props) => {
+  const videoList = props.videos.map((video)=>{
+          return (
+            <VideoListItems onVideoSelect={props.onVideoSelect} key= {video.etag} video={video}/>
+          )
+        })
+  return (
+    <ul className="col-md-4 list-group">
+      {/* {props.videos.map((video)=>{
+        return <VideoListItems video={video}/>
+      })} */}
+      {videoList}
+    </ul>
+  )
+}
+
+export default VideoList;
